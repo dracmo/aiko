668123749950119027f37a5d8415abc8d9d37cd5
dracmo <bouazizwellan@gmail.com>
2024-08-17 17:43:24 +0900

    test5

--- menu.js
+++ menu.js
@@ -3,7 +3,7 @@ $(function () {
   ハンバーガーメニュー
   ===================================================*/
   $(".hamburger").on("click", function () {
-    $("header").toggleClass("open"); 
+    $("header").toggleClass("open");
   });
 
   $("#mask").on("click", function () {
@@ -13,7 +13,6 @@ $(function () {
   $("#navi a").on("click", function () {
     $("header").removeClass("open");
   });
-
 
   $(window).scroll(function () {
     $(".steps").each(function () {
@@ -28,45 +27,46 @@ $(function () {
   });
 });
 
-
-
-
-
-
-  function PageTopAnime() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 100){//上から100pxスクロールしたら
-      $('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
-      $('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
-    }else{
-      if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
-        $('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
-        $('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
-      }
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 100) {
+    //上から100pxスクロールしたら
+    $("#page-top").removeClass("DownMove"); //#page-topについているDownMoveというクラス名を除く
+    $("#page-top").addClass("UpMove"); //#page-topについているUpMoveというクラス名を付与
+  } else {
+    if ($("#page-top").hasClass("UpMove")) {
+      //すでに#page-topにUpMoveというクラス名がついていたら
+      $("#page-top").removeClass("UpMove"); //UpMoveというクラス名を除き
+      $("#page-top").addClass("DownMove"); //DownMoveというクラス名を#page-topに付与
     }
   }
-  
-  // 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function () {
-    PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
-  });
-  
-  // ページが読み込まれたらすぐに動かしたい場合の記述
-  $(window).on('load', function () {
-    PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
-  });
-  
-  
-  // #page-topをクリックした際の設定
-  $('#page-top').click(function () {
-    var scroll = $(window).scrollTop(); //スクロール値を取得
-    if(scroll > 0){
-      $(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
-          $('body,html').animate({
-              scrollTop: 0
-          }, 2000,function(){//スクロールの速さ。数字が大きくなるほど遅くなる
-              $('#page-top').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
-          });	
-    }
-      return false;//リンク自体の無効化
-  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on("load", function () {
+  PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$("#page-top").click(function () {
+  const scroll = $(window).scrollTop(); //スクロール値を取得
+  if (scroll > 0) {
+    $(this).addClass("floatAnime"); //クリックしたらfloatAnimeというクラス名が付与
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      2000,
+      function () {
+        //スクロールの速さ。数字が大きくなるほど遅くなる
+        $("#page-top").removeClass("floatAnisme"); //上までスクロールしたらfloatAnimeというクラス名を除く
+      }
+    );
+  }
+  return false; //リンク自体の無効化
+});
